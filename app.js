b2e18792b8a50290754a06ed4ca2595d3b986a94
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2025-05-28 15:05:35 +0200

    add new message form

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newRouter);
+
 
 const PORT = 3000;
 
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import messages from '../db.js';
 
 const newRouter = Router();
 
 newRouter.get("/", (req, res) => res.render("form"));
+
+newRouter.post("/", (req, res) => {
+    messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+    res.redirect("/");
+})
 
 export default newRouter;
