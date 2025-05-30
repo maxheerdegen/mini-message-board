c7872aec9622fc3bf624e3756910ecfaee4842a0
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2025-05-30 22:31:16 +0200

    add button to display details of single message

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,4 +5,8 @@ const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => res.render("index", {title: "mini messageboard", messages: messages }));
 
+indexRouter.get("/:id", (req, res) => {
+    res.render("message", {messages: messages, id: req.params.id})
+})
+
 export default indexRouter;
